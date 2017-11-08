aa8e5616a04ef0a070b1e3c86c3ba3edf16899a4
Adrian <a.kuchinke@gmail.com>
2017-11-08 22:27:29 +0100

    added index page. fixed #7

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -12,6 +12,10 @@ class AuthorsController < ApplicationController
   def show
   	@author = Author.find(params[:id])
   end
+
+  def show
+  	@author = Author.find(params[:id])
+  end
   private
   def author_params
     params.require(:author).permit(:first_name, :last_name, :homepage)
