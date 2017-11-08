398eba88875d9e53b081158fd2404e01dc3a7bd2
Adrian <a.kuchinke@gmail.com>
2017-11-08 20:20:13 +0100

    created author model for #3

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,7 +1,7 @@
 describe "New Author", type: :model do
 
   it "Author model should have name, lastname and homepage" do
-  	author = Author.new('Alan', 'Turing', 'http://wikipedia.org/Alan_Turing')
+  	author = Author.new(first_name:'Alan', last_name:'Turing', homepage:'http://wikipedia.org/Alan_Turing')
   	expect(author.first_name).to eq('Alan')
   	expect(author.last_name).to eq('Turing')
   	expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
