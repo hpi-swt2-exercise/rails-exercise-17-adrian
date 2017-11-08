9abd6f97817133003e889a0ac62b722fda672d76
Adrian <a.kuchinke@gmail.com>
2017-11-08 15:31:36 +0100

    fixed authors, closing #1

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
  
 
 
+resources :authors
+#resources :papers
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-  get 'new/author' => 'authors#new'
+
  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
