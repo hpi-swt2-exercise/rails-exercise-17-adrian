4693fcd423570fd293ee28333b3513e90566b680
Adrian <a.kuchinke@gmail.com>
2017-11-09 17:56:02 +0100

    failed test for #19

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
+describe "New Paper", type: :model do
+ it "Should validate venue" do
+  	paper = Paper.new(title:'Some title', venue: nil, year:2017)
+  	expect(paper.valid?).to eq(false)
+  
+  end
 
-RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "Should validate title" do
+  	paper = Paper.new(title: nil, venue: "Somewhere", year:2017)
+  	expect(paper.valid?).to eq(false)
+  
+  end
+
 end
