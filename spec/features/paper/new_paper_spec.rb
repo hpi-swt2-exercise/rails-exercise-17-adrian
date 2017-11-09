4d8681233190e25677ffb709a86f2c387ba08177
Adrian <a.kuchinke@gmail.com>
2017-11-09 17:11:05 +0100

    added paper model and new paper page, solved #17

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -5,7 +5,13 @@ describe "New paper page", type: :feature do
   it "should render withour error" do
     visit new_paper_path
   end
-
+it "should have text input for title venu and year" do
+    visit new_author_path
+  
+  expect(page).to have_field('Title')
+  expect(page).to have_field('Venue')
+  expect(page).to have_field('Year')
+  end
  
 
 end
