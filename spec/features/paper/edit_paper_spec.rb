b8d0d173c392ec3f6e0ce55ccd8a3d82ee6f6e6a
Adrian <a.kuchinke@gmail.com>
2017-11-09 19:41:07 +0100

    added edit behavior and test for papers, solved #26

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -8,7 +8,13 @@ describe "Edit paper page", type: :feature do
     visit edit_paper_path(@paper)
   end
 
- 
+  it "Save changed" do
+    visit edit_paper_path(@paper)
+    fill_in 'Title', with: 'A new Title'
+    click_button 'Save Paper'
+    expect(page).to have_content('A new Title')
+  end
+
   
 
 end
