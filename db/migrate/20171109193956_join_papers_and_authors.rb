48e7c7ea006209f57499b95677438b379db428c1
Adrian <a.kuchinke@gmail.com>
2017-11-09 20:44:45 +0100

    migrated to n:m relation, solved #28

---
+++ db/migrate/20171109193956_join_papers_and_authors.rb
@@ -0,0 +1,5 @@
+class JoinPapersAndAuthors < ActiveRecord::Migration
+  def change
+  	create_join_table :authors, :papers
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109163613) do
+ActiveRecord::Schema.define(version: 20171109193956) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "first_name"
@@ -21,15 +21,17 @@ ActiveRecord::Schema.define(version: 20171109163613) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "authors_papers", id: false, force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.integer "paper_id",  null: false
+  end
+
   create_table "papers", force: :cascade do |t|
     t.string   "title"
     t.string   "venue"
     t.integer  "year"
-    t.integer  "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  add_index "papers", ["author_id"], name: "index_papers_on_author_id"
 
 end
